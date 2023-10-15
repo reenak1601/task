74dadadf2e6be508cb109c1577a57c00e4c8dcc5
Reena Kumari <98412837+reenak1601@users.noreply.github.com>
2023-10-15 22:34:00 +0530

    Delete Task Cloud Vandana directory

--- Task Cloud Vandana/JavaScript/reverse.js
+++
@@ -1,9 +0,0 @@
-const str = 'This is sunny day';
-const reverseSentence = str => {
-   const arr = str.split(" ");
-   const reversed = arr.map(el => {
-      return el.split('').reverse().join("");
-   });
-   return reversed.join(" ");
-};
-console.log(reverseSentence(str));
